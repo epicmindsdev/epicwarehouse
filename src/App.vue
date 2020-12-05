1c14ae793e318aae990500674b4d5b99225b9b91
epicmindsdev <info@epicminds.io>
2020-12-05 16:02:29 +0100

    navbar, cards added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navigation">
+      <img class="logo" src="https://morfalto.sirv.com/Stock%20Logo.png" width="200px" height="auto">
+      <router-link class="link" to="/">Product check</router-link> |
+      <router-link class="link" to="/about">Overview</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: whitesmoke;
+  height: 100vh;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.navigation {
+  width: 100%;
+  padding: 30px 10%;
+  background-color: white;
+  text-align: left;
 }
+
+.logo {
+  margin-right: 30px;
+}
+
+.link {
+  align-content: center;
+  padding: 20px 20px 20px 20px;
+}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg=""/>
+    <b-row class="p-details" style="margin-left: auto; margin-right: auto">
+      <b-col class="card-left">wjdjdjdjedjedjejdejdiejdijeidjiwejdewdjewndhkwbgcjvacguvguvacuhbewchbweucbvewbzibebizcbewicbhwbachjlwbchbewh
+      </b-col>
+      <b-col class="card-left">dd</b-col>
+    </b-row>
   </div>
 </template>
 
@@ -16,3 +20,29 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+
+.home{
+
+}
+
+.p-details {
+  max-width: 80%;
+  margin-top: 10px;
+}
+
+.card-left {
+  background-color: white;
+  max-width: 40%;
+  height: fit-content;
+  margin-right: 2.5%;
+}
+
+.card-right {
+  background-color: white;
+  max-width: 40%;
+  height: fit-content;
+  margin-left: 2.5%;
+}
+</style>
