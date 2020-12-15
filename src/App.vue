3d10989ff8fcf63123f676ee8a22cfe863f5120b
epicmindsdev <info@epicminds.io>
2020-12-16 00:28:08 +0100

    added login screen and loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,66 @@
 <template>
+
   <div id="app">
-    <nav class="navigation">
+    <div class="login-container" v-show="this.loginView === 1 || this.loginView === 2">
+      <div class="login-card" v-show="this.loginView === 1">
+        <b-row><img class="logo" src="https://morfalto.sirv.com/Stock%20Logo.png" width="185px" height="26px"></b-row>
+        <b-row class="spacer-m"></b-row>
+        <b-row class="card-title">
+          Username
+        </b-row>
+        <b-row><label>
+          <b-input v-model="username"></b-input>
+        </label></b-row>
+        <div class="spacer-s"></div>
+        <b-row class="card-title">
+          Password
+        </b-row>
+        <b-row><label>
+          <b-input type="password" v-model="password"></b-input>
+        </label></b-row>
+        <b-row>
+          <b-button variant="outline-primary" style="margin-top: 15px; margin-left: auto; margin-right: auto"
+                    v-on:click="login(username, password)">Submit
+          </b-button>
+        </b-row>
 
-      <b-row>
-        <img class="logo" src="https://morfalto.sirv.com/Stock%20Logo.png" width="270px" height="37px">
-        <router-link class="link" to="/">CHECK</router-link>
-        <router-link class="link" to="/about">OVERVIEW</router-link>
-      </b-row>
-
-    </nav>
-    <router-view/>
+      </div>
+      <span class="loader mid" v-show="this.loginView === 2"></span>
+    </div>
+    <div v-show="this.loginView === 3">
+      <nav class="navigation">
+        <b-row>
+          <img class="logo" src="https://morfalto.sirv.com/Stock%20Logo.png" width="185px" height="26px">
+          <router-link class="link" to="/">CHECK</router-link>
+          <router-link class="link" to="/about">OVERVIEW</router-link>
+        </b-row>
+      </nav>
+      <router-view/>
+    </div>
   </div>
 </template>
+<script>
 
+export default {
+  data() {
+    return {
+      loginView: 1,
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function (username, password) {
+      if (username === 'admin' && password === 'admin') {
+        this.loginView = 2
+        setTimeout(() => this.loginView = 3, 2000);
+      }
+    }
+
+  }
+
+}
+</script>
 
 <style lang="scss">
 
@@ -38,6 +86,7 @@
 
 .logo {
   margin-right: 30px;
+  margin-top: 5px;
 }
 
 .link {
@@ -66,4 +115,69 @@
   text-decoration-line: none;
 }
 
+.login-container {
+  background-image: radial-gradient(circle, #74D2FF, #48C3FF, #1BB5FF);
+  width: 100%;
+  height: 100vh;
+  background-color: lightskyblue;
+  padding-top: 20vh;
+  padding-bottom: 20vh;
+}
+
+.login-card {
+  width: 350px;
+  height: 400px;
+  background-color: white;
+  border-radius: 10px;
+  margin: auto;
+  box-shadow: 3px 3px 4px 2px rgba(43, 43, 43, 0.09);
+  padding: 50px 95px 95px 95px;
+}
+
+.spacer-m {
+  height: 6px;
+}
+
+.spacer-m {
+  height: 10px;
+}
+
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  display: inline-block;
+  border-top: 4px solid #FFF;
+  border-right: 4px solid transparent;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+.loader::after {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  border-left: 4px solid #1BB5FF;
+  border-bottom: 4px solid transparent;
+  animation: rotation 0.5s linear infinite reverse;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.mid {
+  margin-top: 25vh;
+
+}
 </style>
